e639984c1b4682d88395a805de35e43c577b381b
dhairyaattri <dhairyaattri16@gmail.com>
2020-09-24 13:36:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  theme: {
+      container: {
+        center: true
+      },
+    extend: {
+      colors: {
+        'blue': '#1DA1F2',
+        'darkblue': '#2795D9',
+        'lightblue': '#EFF9FF',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
+  },
+  variants: {},
+  plugins: [ ],
+}
